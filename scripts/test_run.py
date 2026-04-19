65593a8143daece1dce1b945d4032122431ec2d0
ChitsNits <chitsnits@Mac.Home>
2026-04-19 12:57:34 +0100

    Run the FastAPI
    - Update the config
    - Generate model

--- scripts/test_run.py
+++
@@ -1,7 +0,0 @@
-import os
-print("TEST SCRIPT RUNNING")
-
-print("Current working directory:", os.getcwd())
-
-open("test_output.txt", "w").write("hello")
-print("File written")
--- src/config.py
+++ src/config.py
@@ -1,7 +1,32 @@
-required_features = [
-    "Gender", "Married", "Dependents", "Education", "Self_Employed",
-    "ApplicantIncome", "CoapplicantIncome", "LoanAmount",
-    "Loan_Amount_Term", "Credit_History", "Property_Area"
+# src/config.py
+
+# Categorical and numeric feature lists
+categorical_features = [
+    "Gender",
+    "Married",
+    "Dependents",
+    "Education",
+    "Self_Employed",
+    "Property_Area"
 ]
 
+numeric_features = [
+    "ApplicantIncome",
+    "CoapplicantIncome",
+    "LoanAmount",
+    "Loan_Amount_Term",
+    "Credit_History"
+]
+
+# Combined list for prediction input
+required_features = categorical_features + numeric_features
+
+# Hyperparameter grid for GridSearchCV
+param_grid = {
+    "model__n_estimators": [100, 200],
+    "model__max_depth": [5, 10, None],
+    "model__min_samples_split": [2, 5]
+}
+
+# Path to save the trained model
 model_path = "models/best_model.pkl"
